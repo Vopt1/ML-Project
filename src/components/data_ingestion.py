94e69d8757c2f5b2ac13c243fe0a5b1a62bfa1e4
Vopt1 <vatsalyabarhmatmaj2@gmail.com>
2025-06-17 16:22:51 +0530

    Data Ingestion

---
+++ src/components/data_ingestion.py
@@ -0,0 +1,47 @@
+import os
+import sys
+from src.exception import CustomException
+from src.logger import logging
+import pandas as pd
+
+from sklearn.model_selection import train_test_split
+from dataclasses import dataclass
+
+@dataclass
+class DataIngestionConfig:
+    train_data_path = os.path.join('artifacts',"train.csv")
+    test_data_path = os.path.join('artifacts',"test.csv")
+    raw_data_path = os.path.join('artifacts',"data.xlsx")
+
+class DataIngestion:
+    def __init__(self):
+        self.ingestion_config = DataIngestionConfig()
+
+    def InitiateDataIngestion(self):
+        logging.info('Starting Data Ingestion')
+        try:
+            df = pd.read_excel(r"Notebook\data\flight-price.xlsx")
+            logging.info("Successfully Imported the dataset")
+
+            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
+
+            df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
+
+            logging.info('Train test split Initiated')
+            train_set,test_set = train_test_split(df,test_size=0.15,random_state=21)
+
+            train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
+            test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
+
+            logging.info("Ingestion Completed")
+
+            return (
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path
+            )
+        except Exception as e:
+            raise CustomException(e,sys)  
+        
+if __name__ == '__main__':
+    obj = DataIngestion()
+    obj.InitiateDataIngestion()
